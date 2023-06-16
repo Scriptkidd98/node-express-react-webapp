76cd2fad4fff31e426d570bde3bf8979ed9de4b4
Scriptkidd98 <duyilekan98@gmail.com>
2023-06-16 19:00:09 +0100

    created individual exercise div

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from './Navbar';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Home = () => {
+  const[exercises, setExercises] = useState([])
+  const readData = async () => {
+    try{
+      await axios.get('http://localhost:5000/exercises/')
+      .then(res => {console.log(res.data); setExercises(res.data)})
+      .catch(err => console.log(err))
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    readData()
+  },[])
   return (
     <div>
         <Navbar/>
+        
+        <div className='container' style={{marginTop: '50px', paddingBottom: '50px'}}>
+          {exercises.map(exercise => {
+            return(
+              <div className='each-exercise' key={exercise.id}>
+                <div className='row'>
+                  <div className='col-lg-6 col-md-6 col-sm-g col-xs-6'>
+                    <p className='description'>{exercise.description}</p>
+                  </div>
+                  <div className='col-lg-6 col-md-6 col-sm-g col-xs-6'>
+                    <span style={{float: 'right', fontSize: 'smaller', color: 'grey'}}>Remove</span>
+                  </div>
+                </div>
+                <p style={{color: 'grey'}}>{exercise.duration} mins</p>
+                <p style={{color: 'grey'}}>{exercise.date}</p>
+                <p style={{color: 'grey'}}>{exercise.username}</p>
+            </div>
+            )
+          })}
+        </div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div>
         <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-            <Link to="/" className="navbar-brand">ExcerTracker</Link>
+            <Link to="/" className="navbar-brand">ExcerciseTracker</Link>
             <div className="collpase navbar-collapse">
                 <ul className="navbar-nav mr-auto">
                     <li className="navbar-item">
